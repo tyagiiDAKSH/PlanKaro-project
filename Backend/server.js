52cb9460773d3f1993903bbcfa84c27d036e13be
Daksh Tyagi <dakshtyagi533@gmail.com>
2026-03-24 13:33:55 +0530

    fix require paths

--- Backend/server.js
+++ Backend/server.js
@@ -9,16 +9,16 @@ app.use(express.json());
 app.use(cors());
 
 // DB connect
-mongoose.connect("mongodb+srv://dakshtyagi533_db_user:Tyagi533ji@cluster0.px81j0l.mongodb.net/evendDB")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
 // Routes
-const adminRoutes = require("./routes/adminRoutes");
-const userRoutes = require("./routes/userRoutes");
-const vendorRoutes = require("./routes/vendorRoutes");
-const eventRoutes = require("./routes/eventRoutes");
-const messageRoutes = require("./routes/messageRoutes");
+const adminRoutes = require("./routes/adminRoutes.js");
+const userRoutes = require("./routes/userRoutes.js");
+const vendorRoutes = require("./routes/vendorRoutes.js");
+const eventRoutes = require("./routes/eventRoutes.js");
+const messageRoutes = require("./routes/messageRoutes.js");
 
 app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
