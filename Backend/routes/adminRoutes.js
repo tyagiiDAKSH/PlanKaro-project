1b10d940c4159ccc2f0d8a20c14115903ee0b820
Daksh Tyagi <dakshtyagi533@gmail.com>
2026-03-24 14:24:25 +0530

    added admin register route

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -2,8 +2,29 @@ const express = require("express");
 const router = express.Router();
 const Admin = require("../models/admin");
 
+
+// Testing
 router.get("/", (req, res) => {
   res.send("Admin route working 🚀");
+});
+
+
+// Register admin
+router.post("/register", async (req, res) => {
+  try {
+    const newAdmin = new Admin(req.body);
+    await newAdmin.save();
+
+    res.json({
+      success: true,
+      message: "Admin registered successfully"
+    });
+  } catch (err) {
+    res.json({
+      success: false,
+      message: "Error registering admin"
+    });
+  }
 });
 
 // Admin Login
